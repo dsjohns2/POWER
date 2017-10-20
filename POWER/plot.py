2fdbffdae38fbd1bd2e0f8a51b8dcc55ec8db148
dsjohns2 <dsjohns2@illinois.edu>
2017-10-20 10:06:20 -0500

    Add amplitude and phase plot generation

--- POWER/plot.py
+++ POWER/plot.py
@@ -44,8 +44,12 @@ import sys
 run_name = sys.argv[1]
 
 python_strain = np.loadtxt("./Extrapolated_Strain/"+run_name+"/"+run_name+"_radially_extrapolated_strain.dat")
+python_phase = np.loadtxt("./Extrapolated_Strain/"+run_name+"/"+run_name+"_radially_extrapolated_phase.dat")
+python_amp = np.loadtxt("./Extrapolated_Strain/"+run_name+"/"+run_name+"_radially_extrapolated_amplitude.dat")
 py_t = python_strain[:, 0]
 py_hplus = python_strain[:, 1]
+py_phase = python_phase[:, 1]
+py_amp = python_amp[:, 1]
 
 run_name_without_resolution = run_name.split("_")[0]
 
@@ -57,3 +61,24 @@ plt.xlim((0,None))
 plt.ylabel('${\mathfrak{R}} [h(t)]$')
 plt.xlabel('Time [M]')
 plt.savefig("./Extrapolated_Strain/"+run_name+"/strain_plot.png")
+plt.close()
+
+matplotlib.rcParams.update({'font.size': 12})
+matplotlib.rcParams.update({'font.family': 'serif'})
+plt.title(run_name_without_resolution)
+plt.plot(py_t, py_phase)
+plt.xlim((0,None))
+plt.ylabel('Phase')
+plt.xlabel('Time [M]')
+plt.savefig("./Extrapolated_Strain/"+run_name+"/phase_plot.png")
+plt.close()
+
+matplotlib.rcParams.update({'font.size': 12})
+matplotlib.rcParams.update({'font.family': 'serif'})
+plt.title(run_name_without_resolution)
+plt.plot(py_t, py_amp)
+plt.xlim((0,None))
+plt.ylabel('Amplitude')
+plt.xlabel('Time [M]')
+plt.savefig("./Extrapolated_Strain/"+run_name+"/amplitude_plot.png")
+plt.close()
